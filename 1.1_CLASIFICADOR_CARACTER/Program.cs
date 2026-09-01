d5f53facfca76083a046435a256529d522a8f7fe
Carlos Andres <charliandreth31@gmail.com>
2026-08-31 20:23:16 -0500

    first commit - TALLER DE CARLOS

---
+++ 1.1_CLASIFICADOR_CARACTER/Program.cs
@@ -0,0 +1,21 @@
+﻿char caracterEntrada;
+Console.WriteLine("===========================");
+Console.WriteLine("=== CLASIFICADOR DE CARACTERES ===");
+
+do
+{
+    Console.Write("Ingrese un caracter para determinar si es VOCAL o CONSONANTE : ");
+} while (!char.TryParse(Console.ReadLine(), out caracterEntrada));
+
+caracterEntrada = char.ToLower(caracterEntrada);
+Console.WriteLine("  ");
+if (caracterEntrada == 'a'||caracterEntrada == 'e'||caracterEntrada == 'i'||caracterEntrada == 'o' || caracterEntrada == 'u')
+{
+    Console.WriteLine("El caracter " + caracterEntrada + " es una VOCAL.");
+}
+else
+{
+    Console.WriteLine("El caracter " + caracterEntrada + " es una CONSONANTE.");
+}
+Console.WriteLine("===========================");
+Console.ReadKey();
---
+++ 1.2_AÑO_BISIESTO/Program.cs
@@ -0,0 +1,26 @@
+﻿int anio;
+
+Console.WriteLine("===========================");
+Console.WriteLine("=== DETERMINADOR AÑO BISIESTO ===");
+
+do
+{
+    Console.WriteLine("Ingrese un año para derteminar si es bisiesto: ");
+    
+}while(!int.TryParse(Console.ReadLine(),out anio)||anio <1583||anio>3000);
+if(anio % 4 == 0 && anio % 100 != 0 || anio % 400 == 0)
+{
+    Console.WriteLine("EL AÑO "+anio+" ES BISIESTO");
+
+}
+else
+{
+    Console.WriteLine("EL AÑO "+anio+" NO ES BISIESTO");
+
+}
+Console.ReadKey();
+
+
+
+
+Console.WriteLine("===========================");
