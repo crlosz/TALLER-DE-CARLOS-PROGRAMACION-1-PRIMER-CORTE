14c04323a651af72822f3d2144d6c19d01345dc1
Carlos Andres <charliandreth31@gmail.com>
2026-08-31 20:44:45 -0500

    TALLER DE CARLOS - Ejercicio 1.3 Aprobacion del semestre

---
+++ 1.3_APROBACION_SEMESTRE/Program.cs
@@ -0,0 +1,32 @@
+﻿double nota1, nota2, nota3, promedio;
+Console.WriteLine("===========================");
+Console.WriteLine("=== APROBACION DEL SEMESTRE ===");
+do
+{
+    Console.Write("Ingrese la nota del primer corte (0.0 a 5.0): ");
+} while (!double.TryParse(Console.ReadLine(), out nota1) || nota1 < 0.0 || nota1 > 5.0);
+
+do
+{
+    Console.Write("Ingrese la nota del segundo corte (0.0 a 5.0): ");
+} while (!double.TryParse(Console.ReadLine(), out nota2) || nota2 < 0.0 || nota2 > 5.0);
+
+do
+{
+    Console.Write("Ingrese la nota del tercer corte (0.0 a 5.0): ");
+} while (!double.TryParse(Console.ReadLine(), out nota3) || nota3 < 0.0 || nota3 > 5.0);
+promedio = (nota1 * 0.30) + (nota2 * 0.30) + (nota3 * 0.40);
+promedio = (int)(promedio * 100) / 100.0;
+Console.WriteLine(" ");
+Console.WriteLine("PROMEDIO PONDERADO: " + promedio);
+if (promedio > 3.5 && nota1 >= 3.0 && nota2 >= 3.0 && nota3 >= 3.0)
+{
+    Console.WriteLine("EL ESTUDIANTE HA APROBADO EL SEMESTRE.");
+}
+else
+{
+    Console.WriteLine("EL ESTUDIANTE NO HA APROBADO EL SEMESTRE.");
+}
+
+Console.WriteLine("===========================");
+Console.ReadKey();
