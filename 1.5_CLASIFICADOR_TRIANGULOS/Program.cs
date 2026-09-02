2857edcbed2e88992b5fce2e31e13fcfee2a36d6
Carlos Andres <charliandreth31@gmail.com>
2026-09-01 20:14:55 -0500

    TALLER DE CARLOS - Ejercicios 1.5 y 1.6 terminados

---
+++ 1.5_CLASIFICADOR_TRIANGULOS/Program.cs
@@ -0,0 +1,50 @@
+﻿double ladoA, ladoB, ladoC;
+bool esTriangulo;
+
+Console.WriteLine("===========================");
+Console.WriteLine("=== CLASIFICADOR DE TRIANGULOS ===");
+Console.WriteLine("===========================");
+Console.WriteLine(" ");
+
+do
+{
+    Console.Write("Ingrese el lado A: ");
+} while (!double.TryParse(Console.ReadLine(), out ladoA) || ladoA <= 0);
+
+do
+{
+    Console.Write("Ingrese el lado B: ");
+} while (!double.TryParse(Console.ReadLine(), out ladoB) || ladoB <= 0);
+
+do
+{
+    Console.Write("Ingrese el lado C: ");
+} while (!double.TryParse(Console.ReadLine(), out ladoC) || ladoC <= 0);
+
+Console.WriteLine(" ");
+
+esTriangulo = (ladoA + ladoB > ladoC) && (ladoA + ladoC > ladoB) && (ladoB + ladoC > ladoA);
+
+if (esTriangulo)
+{
+    if (ladoA == ladoB && ladoB == ladoC)
+    {
+        Console.WriteLine(" El triangulo es EQUILATERO (todos sus lados son iguales).");
+    }
+    else if (ladoA == ladoB || ladoA == ladoC || ladoB == ladoC)
+    {
+        Console.WriteLine(" El triangulo es ISOSCELES (tiene dos lados iguales).");
+    }
+    else
+    {
+        Console.WriteLine(" El triangulo es ESCALENO (todos sus lados son diferentes).");
+    }
+}
+else
+{
+    Console.WriteLine("Los lados ingresados NO forman un triangulo valido.");
+    Console.WriteLine("La suma de cualesquiera dos lados debe ser siempre mayor que el tercer lado.");
+}
+
+Console.WriteLine("===========================");
+Console.ReadKey();
---
+++ 1.6_INDICE_MASA_CORPORAL/Program.cs
@@ -0,0 +1,38 @@
+﻿double peso, altura, imc;
+bool esNormal;
+
+Console.WriteLine("===========================");
+Console.WriteLine("=== CALCULADORA DE IMC ===");
+Console.WriteLine(" ");
+
+do
+{
+    Console.Write("Ingrese su peso en kg (ejemplo: 70,5): ");
+} while (!double.TryParse(Console.ReadLine(), out peso) || peso <= 0);
+
+do
+{
+    Console.Write("Ingrese su altura en metros (ejemplo: 1,75): ");
+} while (!double.TryParse(Console.ReadLine(), out altura) || altura <= 0);
+
+imc = peso / (altura * altura);
+
+imc = (int)(imc * 100) / 100.0;
+
+
+esNormal = imc >= 18.5 && imc < 25.0;
+
+Console.WriteLine(" ");
+Console.WriteLine("Tu IMC es: " + imc);
+
+if (esNormal)
+{
+    Console.WriteLine("Tu peso se encuentra en el rango NORMAL.");
+}
+else
+{
+    Console.WriteLine("Tu peso NO se encuentra en el rango normaal");
+}
+
+Console.WriteLine("===========================");
+Console.ReadKey();
